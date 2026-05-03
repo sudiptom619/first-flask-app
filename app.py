29228ce7a3ce6761c632e713734784a631560357
Sudipto <sudiptom619@gmail.com>
2026-05-03 22:34:38 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import uvicorn
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello World"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
