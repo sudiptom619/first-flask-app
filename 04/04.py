0d19b06f70a0289668f64ea0db30bfb3d0f537c0
Sudipto <sudiptom619@gmail.com>
2026-05-05 09:44:02 +0530

    serving static files

---
+++ 04/04.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, Response
+
+app = Flask(
+    __name__, template_folder="templates", static_folder="static", static_url_path="/"
+)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
