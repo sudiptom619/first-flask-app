02dc186feef8fc29892d7099a5a2691774945f15
Sudipto <sudiptom619@gmail.com>
2026-05-06 00:13:23 +0530

    session & cookie management complete

--- 05/05.py
+++ 05/05.py
@@ -1,11 +1,37 @@
-from flask import Flask, render_template, request, Response
+from flask import Flask, render_template, request, Response, session, make_response
 
 app = Flask(__name__, template_folder="templates")
+app.secret_key = "SOME_KEY"
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/set_data")
+def set_data():
+    session["name"] = "Mike"
+    return render_template("index.html", message="Session Data Set!")
+
+
+@app.route("/get_data")
+def get_data():
+    name = session["name"]
+    return render_template("index.html", message=f"Name:{name}")
+
+
+@app.route("/clear_session")
+def clear_session():
+    session.clear()
+    return render_template("index.html", message="Session Cleared!")
+
+
+@app.route("/set_cookie")
+def set_cookie():
+    response = make_response(render_template("index.html"))
+    response.set_cookie("cookie_name", "cookie_value")
+    return response
 
 
 if __name__ == "__main__":
